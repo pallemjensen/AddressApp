beac89e436e634957789eae89fe8399f3311bb47
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-01-29 16:14:45 +0100

    css icon done

--- src/addressapp/MainApp.java
+++ src/addressapp/MainApp.java
@@ -12,6 +12,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -56,6 +57,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+        
+        //Set the application icon
+        this.primaryStage.getIcons().add(new Image("file:resources/images/Address_Book.png"));
 
         initRootLayout();
 
