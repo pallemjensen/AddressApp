baaadd5aaff89adfbf20fd8c587b2497e8f614c6
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-02-01 11:05:42 +0100

    added anno to birthday added xml bday util

--- src/addressapp/model/Person.java
+++ src/addressapp/model/Person.java
@@ -5,6 +5,7 @@
  */
 package addressapp.model;
 
+import addressapp.util.LocalDateAdapter;
 import java.time.LocalDate;
 
 import javafx.beans.property.IntegerProperty;
@@ -13,6 +14,7 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 /**
  * Model class for a Person.
@@ -109,7 +111,8 @@ public class Person {
     public StringProperty cityProperty() {
         return city;
     }
-
+    
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
     public LocalDate getBirthday() {
         return birthday.get();
     }
