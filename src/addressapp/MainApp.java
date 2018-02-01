2005f8389a69705c75c021696e81a6574e1c4f37
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-02-01 10:29:16 +0100

    marshal + unmarshal with JAXB

--- src/addressapp/MainApp.java
+++ src/addressapp/MainApp.java
@@ -1,6 +1,7 @@
 package addressapp;
 
 import addressapp.model.Person;
+import addressapp.model.PersonListWrapper;
 import addressapp.view.PersonEditDialogController;
 import addressapp.view.PersonOverviewController;
 import java.io.File;
@@ -11,11 +12,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 public class MainApp extends Application {
 
@@ -56,7 +62,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
-        
+
         //Set the application icon
         this.primaryStage.getIcons().add(new Image("file:resources/images/Address_Book.png"));
 
@@ -116,71 +122,133 @@ public class MainApp extends Application {
      */
     public boolean showPersonEditDialog(Person person) {
         try {
-         // Load the fxml file and create a new stage for the popup dialog.
-         FXMLLoader loader = new FXMLLoader();
-         loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
-         AnchorPane page = (AnchorPane) loader.load();
-         
-         // Create the dialog Stage.
-         Stage dialogStage = new Stage();
-         dialogStage.setTitle("Edit Person");
-         dialogStage.initModality(Modality.WINDOW_MODAL);
-         dialogStage.initOwner(primaryStage);
-         Scene scene = new Scene(page);
-         dialogStage.setScene(scene);
-         
-          // Set the person into the controller.
-          PersonEditDialogController controller = loader.getController();
-          controller.setDialogStage(dialogStage);
-          controller.setPerson(person);
-          
-          // Show the dialog and wait until the user closes it
-          dialogStage.showAndWait();
-          
-          return controller.isOkClicked();
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
         } catch (IOException ex) {
-          return false; 
+            return false;
         }
     }
-    
+
     /**
- * Returns the person file preference, i.e. the file that was last opened.
- * The preference is read from the OS specific registry. If no such
- * preference can be found, null is returned.
- * 
- * @return
- */
-public File getPersonFilePath() {
-    Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
-    String filePath = prefs.get("filePath", null);
-    if (filePath != null) {
-        return new File(filePath);
-    } else {
-        return null;
+     * Returns the person file preference, i.e. the file that was last opened.
+     * The preference is read from the OS specific registry. If no such
+     * preference can be found, null is returned.
+     *
+     * @return
+     */
+    public File getPersonFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
     }
-}
 
-/**
- * Sets the file path of the currently loaded file. The path is persisted in
- * the OS specific registry.
- * 
- * @param file the file or null to remove the path
- */
-public void setPersonFilePath(File file) {
-    Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
-    if (file != null) {
-        prefs.put("filePath", file.getPath());
+    /**
+     * Sets the file path of the currently loaded file. The path is persisted in
+     * the OS specific registry.
+     *
+     * @param file the file or null to remove the path
+     */
+    public void setPersonFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
 
-        // Update the stage title.
-        primaryStage.setTitle("AddressApp - " + file.getName());
-    } else {
-        prefs.remove("filePath");
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp - " + file.getName());
+        } else {
+            prefs.remove("filePath");
 
-        // Update the stage title.
-        primaryStage.setTitle("AddressApp");
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp");
+        }
     }
-}
-    
+
+    /**
+     * Loads person data from the specified file. The current person data will
+     * be replaced.
+     *
+     * @param file
+     */
+    public void loadPersonDataFromFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
+
+            // Reading XML from the file and unmarshalling.
+            PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+            personData.clear();
+            personData.addAll(wrapper.getPersons());
+
+            // Save the file path to the registry.
+            setPersonFilePath(file);
+
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not load data");
+            alert.setContentText("Could not load data from file:\n" + file.getPath());
+
+            alert.showAndWait();
+        }
+    }
+
+    /**
+     * Saves the current person data to the specified file.
+     *
+     * @param file
+     */
+    public void savePersonDataToFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Wrapping our person data.
+            PersonListWrapper wrapper = new PersonListWrapper();
+            wrapper.setPersons(personData);
+
+            // Marshalling and saving XML to the file.
+            m.marshal(wrapper, file);
+
+            // Save the file path to the registry.
+            setPersonFilePath(file);
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not save data");
+            alert.setContentText("Could not save data to file:\n" + file.getPath());
+
+            alert.showAndWait();
+        }
+    }
+
     /**
      * Returns the main stage.
      *
